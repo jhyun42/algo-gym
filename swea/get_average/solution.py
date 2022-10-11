251ea2decc3fb7cf1d884520585d2d7fefbcc172
jhyun42 <foreatu@gmail.com>
2022-10-11 14:56:32 +0900

    Add get_average

--- swea/get_average/solution.py
+++ swea/get_average/solution.py
@@ -2,8 +2,5 @@ N = int(input())
 
 for test_case in range(1, N + 1):
     input_list = list(map(int, input().split()))
-    ret = 0
-    for num in input_list:
-        if num % 2 == 1:
-            ret += num
+    ret = round(sum(input_list) / len(input_list))
     print('#{} {}'.format(test_case, ret))
